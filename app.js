de81eb59a39a2033d98b24ddc2054dd0170176e5
Aliens-Dev <merazganabil34@gmail.com>
2020-10-20 02:08:10 +0100

    fixed animation

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const sortMethod = document.querySelector('.algo')
 const sortButton = document.getElementById('sort')
 const speed = document.getElementById('speed')
 const speed_value = document.getElementById('speed_value')
+
+const Nblock = document.getElementById('n-block');
 const loading = document.querySelector('.loading')
 
-const list = Array(150).fill().map(()=> Math.floor(Math.random() * 200))
-
-const brickWidth = Number.parseFloat(body.clientWidth / list.length).toFixed(2)
-
+let list =  Array(20).fill().map(()=> Math.floor(Math.random() * body.clientHeight));
+let brickWidth = 0;
 
 // Helpers 
 const sleep = (time) => {
@@ -43,6 +43,7 @@ const selection = async function() {
         }
         swap(min,i)
         await sleep(speed.value)
+
     }
     resetState();
 }
@@ -62,7 +63,7 @@ const selection_reverse = async function() {
 let bubble = async function(){
     let len = list.length;
     for (let i = 0; i < len; i++) {
-        for (let j = 0; j < len - 1; j++) {
+        for (let j = 0; j < len; j++) {
             if (list[j] > list[j + 1]) {
                 swap(j, j + 1)
                 await sleep(speed.value)
@@ -91,16 +92,23 @@ const renderBrick = function (width,height,position){
     brick.style.height = height + 'px';
     brick.style.width = width  + 'px';
     brick.style.position = "absolute";
+    brick.style.transition = 'all '+ speed.value +'ms ease-in-out';
     brick.style.backgroundColor = randomColor();
     brick.style.left = (position * width)+'px';
     return brick;
 }
 
-for(let i=0;i<list.length;i++) {
-    const brick = renderBrick(brickWidth,list[i],i);
-    body.append(brick);
+const renderBody = function() {
+    console.log(list.length);
+    console.log(body.clientWidth)
+    brickWidth = Number.parseFloat(body.clientWidth / list.length).toFixed(2)
+    for(let i=0;i<list.length;i++) {
+        const brick = renderBrick(brickWidth,list[i],i);
+        body.append(brick);
+    }
 }
 
+renderBody()
 // Event listeners 
 sortButton.addEventListener('click', function(e) {
     loading.style.display = 'block';
@@ -124,6 +132,17 @@ const resetState = () => {
 }
 
 speed.addEventListener('change', function(e) {
-    speed_value.textContent = e.target.value
+    speed_value.textContent = e.target.value;
+    let blocks = document.querySelectorAll('.brick')
+    for(let i=0; i< blocks.length;i++) {
+        blocks[i].style.transition = 'all '+ e.target.value +'ms ease-in-out'
+    }
+    console.log(blocks)
 })
 
+Nblock.addEventListener('change', function(e) {
+    list = [];
+    list = Array(parseInt(e.target.value)).fill().map(()=> Math.floor(Math.random() * body.clientHeight));
+    body.innerHTML = '';
+    renderBody();
+});
